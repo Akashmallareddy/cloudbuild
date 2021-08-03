7a26d8db998adb561c0820b3417262912635000f
Akash Mallareddy <47551241+Akashmallareddy@users.noreply.github.com>
2021-08-03 16:17:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Hello, This is Akash</h3>"
+    html = "<h3>Hello, World!</h3>"
     return html
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0', port=8080)
